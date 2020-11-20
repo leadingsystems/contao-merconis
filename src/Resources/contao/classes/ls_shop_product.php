7c55c1fe7ccb39ce3a1762486089df9bd869db9e
Volker Stuber <stuber@leadingsystems.de>
2020-11-20 02:40:38 +0100

    Improve variant selector (WIP)

--- src/Resources/contao/classes/ls_shop_product.php
+++ src/Resources/contao/classes/ls_shop_product.php
@@ -1643,10 +1643,11 @@ This method takes an array holding attribute ids as keys and attribute value ids
 This method takes an array holding attribute ids as keys and attribute value ids as values and returns the variant object of the matching variant (the first match if more than one variant matches) or null if no variant matches. If the provided array holds an empty value for an attribute, no variant will match.
 				 */
 				:
-				$args = ls_shop_generalHelper::setArrayLength($args, 1, array());
-				$arr_requestedAttributeValues = $args[0];
+				$args = ls_shop_generalHelper::setArrayLength($args, 2);
+				$arr_requestedAttributeValues = is_array($args[0]) ? $args[0] : [];
+                $bln_returnFirstMatch = (bool) $args[1];
 
-				return $this->_getVariantsByAttributeValues($arr_requestedAttributeValues, true);
+				return $this->_getVariantsByAttributeValues($arr_requestedAttributeValues, $bln_returnFirstMatch);
 				break;
 
 			case '_getVariantsByAttributeValues'
