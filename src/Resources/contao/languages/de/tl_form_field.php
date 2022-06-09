bae9ca61aab5d3d2f842a02582e9ae38d54bfbd2
Volker Stuber <stuber@leadingsystems.de>
2022-06-09 16:29:04 +0200

    Text modifications

--- src/Resources/contao/languages/de/tl_form_field.php
+++ src/Resources/contao/languages/de/tl_form_field.php
@@ -6,11 +6,11 @@
 
 	$GLOBALS['TL_LANG']['tl_form_field']['lsShop_mandatoryOnConditionField'] = array('Pflichtfeld-Prüfung abhängig von anderem Feld');
 	$GLOBALS['TL_LANG']['tl_form_field']['lsShop_mandatoryOnConditionValue'] = array('Wert, von dem die Pflichtfeld-Prüfung abhängt');
-    $GLOBALS['TL_LANG']['tl_form_field']['lsShop_mandatoryOnConditionBoolean'] = array('Pflichtfeld wenn nicht vorhanden');
+    $GLOBALS['TL_LANG']['tl_form_field']['lsShop_mandatoryOnConditionBoolean'] = array('Pflichtfeld, wenn Bedingung nicht zutrifft');
 
-    $GLOBALS['TL_LANG']['tl_form_field']['lsShop_ShowOnConditionField'] = array('Sichtbarkein-Prüfung abhängig von Feld');
-    $GLOBALS['TL_LANG']['tl_form_field']['lsShop_ShowOnConditionValue'] = array('Wert, von dem die Sichtbarkeit-Prüfung abhängt');
-    $GLOBALS['TL_LANG']['tl_form_field']['lsShop_ShowOnConditionBoolean'] = array('Sichtbar wenn nicht vorhanden');
+    $GLOBALS['TL_LANG']['tl_form_field']['lsShop_ShowOnConditionField'] = array('Sichtbarkeit abhängig von Feld');
+    $GLOBALS['TL_LANG']['tl_form_field']['lsShop_ShowOnConditionValue'] = array('Wert, von dem die Sichtbarkeit abhängt');
+    $GLOBALS['TL_LANG']['tl_form_field']['lsShop_ShowOnConditionBoolean'] = array('Sichtbar, wenn Bedingung nicht zutrifft');
 
 	/*
 	 * Misc
