efdcf6826d2df04c82ae419a1d8a9d9cb3c7d382
Rainer Blechinger <57102323+superMi88@users.noreply.github.com>
2022-06-21 00:56:00 +0200

    country-states (#110)
    * WIP
    
    * add lsShop_mandatoryOnConditionField2
    
    * improve form settings
    
    * Improve code to only add the conditional "mandatory fields" to palettes which already include the regular "mandatory field"
    
    * Text modifications
    
    Co-authored-by: Volker Stuber <stuber@leadingsystems.de>

--- src/Resources/contao/languages/en/tl_form_field.php
+++ src/Resources/contao/languages/en/tl_form_field.php
@@ -3,9 +3,15 @@
 	 * Fields
 	 */
 	$GLOBALS['TL_LANG']['tl_form_field']['lsShop_addEmptyOption']									=  array('Add an empty option');
+
 	$GLOBALS['TL_LANG']['tl_form_field']['lsShop_mandatoryOnConditionField'] = array('Mandatory field validation depends on other field');
 	$GLOBALS['TL_LANG']['tl_form_field']['lsShop_mandatoryOnConditionValue'] = array('Mandatory field validation depends on other field value');
-	
+    $GLOBALS['TL_LANG']['tl_form_field']['lsShop_mandatoryOnConditionBoolean'] = array('Mandatory if condition does not apply');
+
+    $GLOBALS['TL_LANG']['tl_form_field']['lsShop_ShowOnConditionField'] = array('Visibility depends on field');
+    $GLOBALS['TL_LANG']['tl_form_field']['lsShop_ShowOnConditionValue'] = array('Visibility depends on value');
+    $GLOBALS['TL_LANG']['tl_form_field']['lsShop_ShowOnConditionBoolean'] = array('Visible if condition does not apply');
+
 	/*
 	 * Misc
 	 */
