607cddb8a1a75e3d8099ee8257438f0a1b1dc642
Daniel Bitsch <bitsch@leadingsystems.de>
2023-03-13 17:12:15 +0100

    fix ls_getRecommendedProductsSelection() if ->lsShopProductRecommendedProducts == NULL

--- src/Resources/contao/classes/ls_shop_cross_seller.php
+++ src/Resources/contao/classes/ls_shop_cross_seller.php
@@ -397,7 +397,10 @@ class ls_shop_cross_seller
         if (is_object($this->ls_currentProductInDetailMode)) {
             $arrProducts = deserialize($this->ls_currentProductInDetailMode->lsShopProductRecommendedProducts);
         }
-        if (count($arrProducts) == 1 && !$arrProducts[0]) {
+        if (
+            !is_array($arrProducts)
+            || (count($arrProducts) == 1 && !$arrProducts[0])
+        ) {
             $arrProducts = array();
         }
         return $arrProducts;
