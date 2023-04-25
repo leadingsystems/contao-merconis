f8d1ce1f996c4063edefd6b65b7faf92bd7e19e6
Daniel Bitsch <bitsch@leadingsystems.de>
2023-04-25 10:43:56 +0200

    Improve insert tag "shopPicture"

--- src/Resources/contao/classes/ls_shop_customInserttags.php
+++ src/Resources/contao/classes/ls_shop_customInserttags.php
@@ -147,13 +147,16 @@ class ls_shop_customInserttags
                         switch ($key)
                         {
                             case 'size':
+                                /*
+                                 * Determine the image size id with the given merconis_alias and replace the alias with
+                                 * the id in the parameter string or remove the size parameter entirely if no image size
+                                 * record could be found with the merconis_alias.
+                                 */
                                 $result = \Database::getInstance()->prepare("SELECT id FROM tl_image_size WHERE merconis_alias=?")->execute($value)->fetchAssoc();
-                                if($result){
-                                    //replace text with id
-                                    $params = str_replace('size='.$value, 'size='.$result['id'], $params);
-                                }else{
-                                    //if no if can be found remove size
-                                    $params = str_replace('size='.$value, '', $params);
+                                if ($result) {
+                                    $params = str_replace('size=' . $value, 'size=' . $result['id'], $params);
+                                } else {
+                                    $params = ls_shop_generalHelper::removeGetParametersFromUrl($params, 'size');
                                 }
                                 break;
                         }
