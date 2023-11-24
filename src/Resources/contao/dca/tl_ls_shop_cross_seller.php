976b6ff47081e27cec4a1521b08e4589476061f5
Rainer Blechinger <57102323+superMi88@users.noreply.github.com>
2023-11-24 11:02:27 +0100

    remove deprecated function

--- src/Resources/contao/dca/tl_ls_shop_cross_seller.php
+++ src/Resources/contao/dca/tl_ls_shop_cross_seller.php
@@ -18,7 +18,7 @@ $GLOBALS['TL_DCA']['tl_ls_shop_cross_seller'] = array(
 		'oncopy_callback' => array(
 			array('Merconis\Core\ls_shop_generalHelper', 'saveLastBackendDataChangeTimestamp')
 		),
-		'onrestore_callback' => array(
+		'onrestore_version_callback' => array(
 			array('Merconis\Core\ls_shop_generalHelper', 'saveLastBackendDataChangeTimestamp')
 		),
         'sql' => array
--- src/Resources/contao/dca/tl_ls_shop_export.php
+++ src/Resources/contao/dca/tl_ls_shop_export.php
@@ -18,7 +18,7 @@ $GLOBALS['TL_DCA']['tl_ls_shop_export'] = array(
         'oncopy_callback' => array(
             array('Merconis\Core\ls_shop_generalHelper', 'saveLastBackendDataChangeTimestamp')
         ),
-        'onrestore_callback' => array(
+        'onrestore_version_callback' => array(
             array('Merconis\Core\ls_shop_generalHelper', 'saveLastBackendDataChangeTimestamp')
         ),
         'sql' => array
