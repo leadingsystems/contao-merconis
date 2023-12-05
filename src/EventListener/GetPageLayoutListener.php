26f3fe267fa461ea35c98c8f01dd1d74b0363cb5
Daniel Bitsch <bitsch@leadingsystems.de>
2023-12-05 08:40:29 +0100

    porting pull request #270

---
+++ src/EventListener/GetPageLayoutListener.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace LeadingSystems\MerconisBundle\EventListener;
+
+use Contao\LayoutModel;
+use Contao\PageModel;
+use Contao\PageRegular;
+use Contao\StringUtil;
+use Merconis\Core\ls_shop_generalHelper;
+
+class GetPageLayoutListener
+{
+    public function getLayoutSettingsForGlobalUse(PageModel $pageModel, LayoutModel $layout, PageRegular $pageRegular): void
+    {
+        $GLOBALS['merconis_globals']['layoutID'] = $layout->id;
+        $GLOBALS['merconis_globals']['layoutName'] = $layout->name;
+        $GLOBALS['merconis_globals']['ls_shop_activateFilter'] = $layout->ls_shop_activateFilter;
+        $GLOBALS['merconis_globals']['ls_shop_useFilterInStandardProductlist'] = $layout->ls_shop_useFilterInStandardProductlist;
+        $GLOBALS['merconis_globals']['ls_shop_numFilterFieldsInSummary'] = $layout->ls_shop_numFilterFieldsInSummary;
+        $GLOBALS['merconis_globals']['ls_shop_useFilterMatchEstimates'] = $layout->ls_shop_useFilterMatchEstimates;
+        $GLOBALS['merconis_globals']['ls_shop_matchEstimatesMaxNumProducts'] = $layout->ls_shop_matchEstimatesMaxNumProducts;
+        $GLOBALS['merconis_globals']['ls_shop_matchEstimatesMaxFilterValues'] = $layout->ls_shop_matchEstimatesMaxFilterValues;
+        $GLOBALS['merconis_globals']['ls_shop_useFilterInProductDetails'] = $layout->ls_shop_useFilterInProductDetails;
+        $GLOBALS['merconis_globals']['ls_shop_hideFilterFormInProductDetails'] = $layout->ls_shop_hideFilterFormInProductDetails;
+
+        $arr_themeData = ls_shop_generalHelper::ls_shop_getThemeDataForID($layout->pid);
+        $GLOBALS['merconis_globals']['contaoThemeFolders'] = isset($arr_themeData) ? StringUtil::deserialize($arr_themeData['folders'], true) : array();
+
+        $GLOBALS['merconis_globals']['int_rootPageId'] = $pageModel->rootId;
+        $arr_pageData = ls_shop_generalHelper::ls_shop_getPageDataForID($pageModel->rootId);
+
+        $GLOBALS['merconis_globals']['ls_shop_decimalsSeparator'] = $arr_pageData['ls_shop_decimalsSeparator'];
+        $GLOBALS['merconis_globals']['ls_shop_thousandsSeparator'] = $arr_pageData['ls_shop_thousandsSeparator'];
+        $GLOBALS['merconis_globals']['ls_shop_currencyBeforeValue'] = $arr_pageData['ls_shop_currencyBeforeValue'];
+    }
+
+    /*
+     * This function checks if we are on a product detail page and if we are,
+     * it checks if the page has different layout settings for the details view
+     * and if it has, it overwrites the page's regular layout settings
+     */
+    public function switchTemplateInDetailsViewIfNecessary(PageModel &$pageModel, LayoutModel &$layout, PageRegular $pageRegular): void
+    {
+        if (!\Input::get('product')) {
+            /*
+             * We don't have to deal with different layouts because we are
+             * not on a product details page
+             */
+            return;
+        }
+
+        $int_layout = $pageModel->lsShopIncludeLayoutForDetailsView ? $pageModel->lsShopLayoutForDetailsView : false;
+
+        if ($pageModel->type != 'root') {
+            $int_pid = $pageModel->pid;
+            $str_type = $pageModel->type;
+            $objParentPage = \PageModel::findParentsById($int_pid);
+
+            if ($objParentPage !== null) {
+                while ($int_pid > 0 && $str_type != 'root' && $objParentPage->next()) {
+                    $int_pid = $objParentPage->pid;
+                    $str_type = $objParentPage->type;
+
+                    if ($objParentPage->lsShopIncludeLayoutForDetailsView) {
+                        if ($int_layout === false) {
+                            $int_layout = $objParentPage->lsShopLayoutForDetailsView;
+                        }
+                    }
+                }
+            }
+        }
+
+        if ($int_layout === false) {
+            /*
+             * We don't have to consider different layouts
+             */
+            return;
+        }
+        $pageModel->layout = $int_layout !== false ? $int_layout : $pageModel->layout;
+
+        $layout = ls_shop_generalHelper::merconis_getPageLayout($pageModel);
+    }
+}
---
+++ src/EventSubscriber/LsjsInsertionSubscriber.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace LeadingSystems\MerconisBundle\EventSubscriber;
+
+
+use Contao\Config;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\Routing\ScopeMatcher;
+use Contao\CoreBundle\Security\Authentication\Token\TokenChecker;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerArgumentsEvent;
+
+class LsjsInsertionSubscriber implements EventSubscriberInterface
+{
+    private ScopeMatcher $scopeMatcher;
+    private ContaoFramework $framework;
+    private string $webDir;
+    private string $projectDir;
+    private TokenChecker $tokenChecker;
+
+    public function __construct(ContaoFramework $framework, ScopeMatcher $scopeMatcher, TokenChecker $tokenChecker, string $webDir, string $projectDir)
+    {
+        $this->framework = $framework;
+        $this->scopeMatcher = $scopeMatcher;
+        $this->webDir = $webDir;
+        $this->projectDir = $projectDir;
+        $this->tokenChecker = $tokenChecker;
+    }
+
+    public function onKernelControllerArguments(ControllerArgumentsEvent $event)
+    {
+        if ($this->scopeMatcher->isBackendMainRequest($event) && $this->tokenChecker->hasBackendUser()) {
+            require_once($this->projectDir . '/assets/lsjs/core/appBinder/binderController.php');
+
+            $arr_config = [
+                'pathForRenderedFiles' => $this->projectDir . '/assets/js',
+                "pathToApp" => $this->webDir . '/bundles/leadingsystemsmerconis/js/lsjs/backend/app',
+                "includeCore" => 'no',
+                "includeCoreModules" => 'no',
+                "debug" => (Config::get('ls_shop_lsjsDebugMode') ? '1' : ''),
+                "no-minifier" => (Config::get('ls_shop_lsjsNoMinifierMode') ? '1' : ''),
+            ];
+
+            $binderController = new \lsjs_binderController($arr_config);
+
+            $GLOBALS['TL_JAVASCRIPT'][] = str_replace($this->projectDir, '', $binderController->getPathToRenderedFile());
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            ControllerArgumentsEvent::class => 'onKernelControllerArguments',
+        ];
+    }
+}
