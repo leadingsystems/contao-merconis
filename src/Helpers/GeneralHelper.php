5cf9c5535229eefa78003fd6751018abf5a78869
vstuber <stuber@leadingsystems.de>
2023-12-20 15:52:41 +0100

    Parse ip ranges when checking ip address to determine whether to bypass referrer check

--- src/Helpers/GeneralHelper.php
+++ src/Helpers/GeneralHelper.php
@@ -7,6 +7,14 @@ use Contao\Environment;
 
 class GeneralHelper
 {
+    private IpChecker $ipChecker;
+
+    public function __construct(IpChecker $ipChecker)
+    {
+
+        $this->ipChecker = $ipChecker;
+    }
+
     public function check_refererCheckCanBeBypassed()
     {
         $str_ipWhitelist = Config::get('ls_shop_ipWhitelist');
@@ -21,9 +29,14 @@ class GeneralHelper
 
         $arr_allowedIpAddresses = array_map('trim', explode(',', $str_ipWhitelist));
 
-        if ((isset($_SERVER['REMOTE_ADDR']) && in_array($_SERVER['REMOTE_ADDR'], $arr_allowedIpAddresses))) {
-            return true;
-        } else if ($str_urlWhitelist && strlen($str_urlWhitelist) > 2) {
+        foreach ($arr_allowedIpAddresses as $allowedIpAddressOrRange) {
+            list($allowedIpStart, $allowedIpEnd) = $this->ipChecker->parseIpRange($allowedIpAddressOrRange);
+            if ($this->ipChecker->isIpInRange($_SERVER['REMOTE_ADDR'] ?? '', $allowedIpStart, $allowedIpEnd)) {
+                return true;
+            }
+        }
+
+        if ($str_urlWhitelist && strlen($str_urlWhitelist) > 2) {
             if (preg_match($str_urlWhitelist, Environment::get('request'))) {
                 return true;
             }
