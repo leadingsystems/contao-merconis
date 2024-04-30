1494f23416c03864cd5ab0892ed4c89b6fd790f2
vstuber <stuber@leadingsystems.de>
2024-04-30 13:32:09 +0200

    Add new Merconis Hook "manipulateLiveHit" (#357)
    * Add new Merconis Hook "manipulateLiveHit"

--- src/Resources/contao/frontendModules/ModuleProductSearch.php
+++ src/Resources/contao/frontendModules/ModuleProductSearch.php
@@ -155,6 +155,13 @@ class ModuleProductSearch extends \Module {
 									$arrHit[$liveHitField] = \Controller::replaceInsertTags($objProduct->{$liveHitField});
 									break;
 							}
+
+                            if (isset($GLOBALS['MERCONIS_HOOKS']['manipulateLiveHit']) && is_array($GLOBALS['MERCONIS_HOOKS']['manipulateLiveHit'])) {
+                                foreach ($GLOBALS['MERCONIS_HOOKS']['manipulateLiveHit'] as $mccb) {
+                                    $objMccb = \System::importStatic($mccb[0]);
+                                    $arrHit = $objMccb->{$mccb[1]}($arrHit, $objProduct);
+                                }
+                            }
 						}
 
 						$arrProducts[] = $arrHit;
