ba4db78be64c1f54de1eedf755c1c7318153a95d
vstuber <stuber@leadingsystems.de>
2024-05-03 04:32:44 +0200

    Improve texts for collective order

--- src/Resources/contao/languages/de/shk.php
+++ src/Resources/contao/languages/de/shk.php
@@ -1,7 +1,7 @@
 <?php
 
-$GLOBALS['TL_LANG']['shk']['text001'] = 'Es befindet sich bereits ein Sammelkaufprodukt im Warenkorb';
-$GLOBALS['TL_LANG']['shk']['text002'] = 'Es befindet sich ein Sammelkauf im Warenkorb, kann nicht mit normalen Produkten kombiniert werden';
-$GLOBALS['TL_LANG']['shk']['text003'] = 'Es befindet sich ein Produkt im Warenkorb, kann nicht mit Sammelkauf kombiniert werden';
-$GLOBALS['TL_LANG']['shk']['text004'] = 'Die Mindestbestellmenge wurde nicht erreicht';
-$GLOBALS['TL_LANG']['shk']['text005'] = 'Die Mindestbestellmenge unterschritten und als Bestellmenge eingetragen.';
+$GLOBALS['TL_LANG']['shk']['text001'] = 'Es befindet sich bereits ein Sammelkaufprodukt im Warenkorb. <span class="add-to-cart-message-subtitle">Bitte schließen Sie diese Sammelkaufbestellung ab, bevor Sie am nächsten Sammelkauf teilnehmen.</span>';
+$GLOBALS['TL_LANG']['shk']['text002'] = 'Es befindet sich ein Sammelkaufprodukt im Warenkorb. <span class="add-to-cart-message-subtitle">Bitte schließen Sie diese Sammelkaufbestellung ab, bevor Sie reguläre Produkte in den Warenkorb legen.</span>';
+$GLOBALS['TL_LANG']['shk']['text003'] = 'Es befindet sich ein reguläres Produkt im Warenkorb. <span class="add-to-cart-message-subtitle">Bitte schließen Sie Ihre Bestellung ab, bevor Sie ein Sammelkaufprodukt in den Warenkorb legen.</span>';
+$GLOBALS['TL_LANG']['shk']['text004'] = 'Die eingegebene Menge ist zu gering.  <span class="add-to-cart-message-subtitle">Bitte beachten Sie die Mindestabnahmemenge.</span>';
+$GLOBALS['TL_LANG']['shk']['text005'] = 'Die Mindestabnahmemenge wurde durch Ihre Eingabe unterschritten. Die Bestellmenge wurde auf die Mindestabnahmemenge gesetzt.';
