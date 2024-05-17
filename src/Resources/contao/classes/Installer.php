a923ed71ab3f7a0e01e462295c5c621778979579
Daniel Bitsch <bitsch@leadingsystems.de>
2024-05-17 11:26:02 +0200

    optimize code

--- src/Resources/contao/classes/Installer.php
+++ src/Resources/contao/classes/Installer.php
@@ -108,17 +108,15 @@ class Installer
                 <?php echo !$blnMerconisFallbackFlagSet ? $GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['installToolMessage10'] : ''; ?>
                 <?php echo sprintf($GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['installToolMessage23'], $GLOBALS['TL_CONFIG']['merconis_serviceNumber']); ?>
                 <ul>
-                    <li>contao-api (<?= InstalledVersions::getPrettyVersion('leadingsystems/contao-api') ?>)</li>
-                    <li>contao-cajax (<?= InstalledVersions::getPrettyVersion('leadingsystems/contao-cajax') ?>)</li>
-                    <li>contao-datacollector (<?= InstalledVersions::getPrettyVersion('leadingsystems/contao-datacollector') ?>)</li>
-                    <li>contao-helpers (<?= InstalledVersions::getPrettyVersion('leadingsystems/contao-helpers') ?>)</li>
-                    <li>contao-lscss4c (<?= InstalledVersions::getPrettyVersion('leadingsystems/contao-lscss4c') ?>)</li>
-                    <li>contao-lsjs4c (<?= InstalledVersions::getPrettyVersion('leadingsystems/contao-lsjs4c') ?>)</li>
-                    <li>contao-merconis (<?= InstalledVersions::getPrettyVersion('leadingsystems/contao-merconis') ?>)</li>
-                    <li>lsce (<?= InstalledVersions::getPrettyVersion('leadingsystems/lsce') ?>)</li>
-                    <li>lscss (<?= InstalledVersions::getPrettyVersion('leadingsystems/lscss') ?>)</li>
-                    <li>lsjs (<?= InstalledVersions::getPrettyVersion('leadingsystems/lsjs') ?>)</li>
-                    <li>merconis-languageselector (<?= InstalledVersions::getPrettyVersion('leadingsystems/merconis-languageselector') ?>)</li>
+                    <?php
+                    $strNeedle = 'leadingsystems';
+                    foreach (InstalledVersions::getInstalledPackages() as $strPackage) {
+                        if (str_contains($strPackage,$strNeedle))
+                        {
+                            echo '<li>'.str_replace($strNeedle.'/','',$strPackage).' ('. InstalledVersions::getPrettyVersion($strPackage).')</li>';
+                        }
+                    }
+                    ?>
                 </ul>
             </div>
             <?php
