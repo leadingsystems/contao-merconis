39e133a19cb370b157192d6bddf8332d98c82fa1
Rainer Blechinger <57102323+superMi88@users.noreply.github.com>
2024-05-22 13:31:30 +0200

    add version check

--- src/Migration/WrapperMigration.php
+++ src/Migration/WrapperMigration.php
@@ -2,6 +2,7 @@
 
 namespace LeadingSystems\MerconisBundle\Migration;
 
+use Composer\InstalledVersions;
 use Contao\CoreBundle\Migration\AbstractMigration;
 use Contao\CoreBundle\Migration\MigrationResult;
 use Doctrine\DBAL\Connection;
@@ -25,6 +26,13 @@ class WrapperMigration extends AbstractMigration
 
     public function shouldRun(): bool
     {
+        if(version_compare(
+            InstalledVersions::getPrettyVersion('leadingsystems/contao-merconis'),
+            "5.1.0", //TODO: change Version to the version needed
+            '<'
+        )){
+            return false;
+        }
 
         $stmt = $this->connection->prepare("
             SELECT *
