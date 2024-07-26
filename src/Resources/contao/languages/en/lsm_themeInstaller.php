a1b7ccc3aab5802611ea046ba17d5ba142d8e76a
Rainer Blechinger <57102323+superMi88@users.noreply.github.com>
2024-07-26 16:20:20 +0200

    restore old Merconis dashboard #387
    * change dashboard view
    
    * error correction

--- src/Resources/contao/languages/en/lsm_themeInstaller.php
+++ src/Resources/contao/languages/en/lsm_themeInstaller.php
@@ -2,10 +2,12 @@
 $GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_001'] = 'Theme Setup';
 $GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_002'] = 'To set up Merconis, you must first create an API key for security reasons. Please select the menu item "API-Key" in the navigation on the left in the "LS API" section and follow the instructions on this page. Once you have created the API key, click on the "Setup" menu item again.';
 $GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_003'] = 'Please run the <a href="%s">Contao Installation Tool</a> now to update the database.';
-$GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_004'] = 'Merconis is fully set up!';
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_004'] = 'Merconis %s is fully set up!';
 $GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_005'] = 'Your service number is "%s". Please always have the service number ready when contacting Merconis techSupport. Important: Do not remove this number from your localconfig file under any circumstances, as Merconis cannot be operated without a valid service number.';
 $GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_006'] = 'There is more than one theme extension installed.';
 $GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_007'] = 'The setup is only possible with exactly one theme extension installed. Please uninstall all unnecessary theme extensions.';
 $GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_008'] = 'Please install a theme extension to perform the automatic setup and use Merconis.';
 $GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_009'] = 'Set up theme!';
 $GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_010'] = 'Theme extension %s is installed and ready for setup.';
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_011'] = 'You can view the shop in the frontend <a href="%s">HERE</a>';
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_012'] = 'You can now start customizing your shop. We recommend that you take a look at the relevant checklist in the manual.';
