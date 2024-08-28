7558ebc936554ec9a4221ce1341274bf94e7ba6d
Rainer Blechinger <57102323+superMi88@users.noreply.github.com>
2024-08-28 09:31:26 +0200

    wip

--- src/Resources/contao/backendModules/dashboard.php
+++ src/Resources/contao/backendModules/dashboard.php
@@ -2,6 +2,7 @@
 
 namespace Merconis\Core;
 
+use Composer\InstalledVersions;
 use Contao\BackendModule;
 
 class dashboard extends BackendModule {
@@ -10,8 +11,26 @@ class dashboard extends BackendModule {
 	
 	
 	protected function compile() {
-        $obj_themeInstaller = ThemeInstaller::getInstance();
-        $this->Template->str_themeInstallerOutput = $obj_themeInstaller->parse();
+
+
+        if(
+            !empty(ls_shop_generalHelper::getInstalledThemeExtensions()) &&
+            !!in_array('leadingsystems/merconis-theme-installer', InstalledVersions::getInstalledPackages() )
+        )
+        {
+            //legacy way if no theme installer or theme extension is installed
+
+            $obj_themeInstaller = Installer_legacy::getInstance();
+            $this->Template->str_themeInstallerOutput = $obj_themeInstaller->parse();
+        }else{
+
+
+            $obj_themeInstaller = ThemeInstaller::getInstance();
+            $this->Template->str_themeInstallerOutput = $obj_themeInstaller->parse();
+        }
+
+
+
 	}
 }
 ?>
