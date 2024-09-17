652b219fbfb27018fc2ba7a30760522f7ed9c5da
Daniel Bitsch <bitsch@leadingsystems.de>
2024-09-17 12:05:22 +0200

    add text

--- src/Resources/contao/languages/de/lsm_themeInstaller.php
+++ src/Resources/contao/languages/de/lsm_themeInstaller.php
@@ -6,16 +6,14 @@ $GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_004'] = 'M
 $GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_005'] = 'Ihre Servicenummer lautet "%s". Bitte halten Sie die Servicenummer bei Anfragen an den Merconis techSupport stets bereit. Wichtig: Entfernen Sie diese Nummer unter keinen Umständen aus Ihrer localconfig-Datei, da der Betrieb von Merconis ohne eine gültige Servicenummer nicht möglich ist.';
 $GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_006'] = 'Es ist mehr als eine Theme-Erweiterung installiert.';
 $GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_007'] = 'Die Einrichtung ist nur mit exakt einer installierten Theme-Erweiterung möglich. Bitte deinstallieren Sie alle überflüssigen Theme-Erweiterungen.';
-$GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_008'] = '
-Bitte installieren Sie eine Theme-Erweiterung, um die automatische Einrichtung durchführen und Merconis benutzen zu können.
+$GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_008'] = 'Bitte installieren Sie eine Theme-Erweiterung, um die automatische Einrichtung durchführen und Merconis benutzen zu können.
 <p>Eine aktuelle Version unseres Themes können Sie unter folgendem Link runter laden und über den Contao Manager installieren:</p>
 <p>
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+<svg class="icon square-up-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
 <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M384 32c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96C0 60.7 28.7 32 64 32l320 0zM160 160c-6.5 0-12.3 3.9-14.8 9.9s-1.1 12.9 3.5 17.4l40 40-71 71C114 302 112 306.9 112 312s2 10 5.7 13.7l36.7 36.7c3.6 3.6 8.5 5.7 13.7 5.7s10-2 13.7-5.7l71-71 40 40c4.6 4.6 11.5 5.9 17.4 3.5s9.9-8.3 9.9-14.8l0-144c0-8.8-7.2-16-16-16l-144 0z"/></svg>
-<a href="%s" target="_blank" rel="noreferrer noopener">Theme-Download</a>
+<a class="theme-download" href="%s" target="_blank" rel="noreferrer noopener">Theme-Download</a>
 </p>
-<p>Eine genauere Anleitung, sowie weitere Konfigurationsmöglichkeiten, finden Sie ebenfalls unter diesem Link.</p>
-';
+<p>Eine genauere Anleitung, sowie weitere Konfigurationsmöglichkeiten, finden Sie ebenfalls unter diesem Link.</p>';
 $GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_009'] = 'Theme einrichten!';
 $GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_010'] = 'Theme-Erweiterung %s ist installiert und zur Einrichtung bereit.';
 $GLOBALS['TL_LANG']['MSC']['ls_shop']['systemMessages']['themeInstall_011'] = 'Sie können den Shop im Frontend <a href="%s">HIER</a> anschauen.';
