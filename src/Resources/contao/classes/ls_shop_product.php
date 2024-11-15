010a991447ad2cd3c16b905ed6d2cda4ca50d4ce
Rainer Blechinger <57102323+superMi88@users.noreply.github.com>
2024-11-15 13:16:37 +0100

    check if Product can be found on a published side

--- src/Resources/contao/classes/ls_shop_product.php
+++ src/Resources/contao/classes/ls_shop_product.php
@@ -2344,11 +2344,19 @@ This method can be used to call a function hooked with the "callingHookedProduct
             $objProductPage = $objPage;
         } else {
             /*-->
-             * Wenn nein, so zielt der Link auf die erstbeste, dem Produkt zugeordnete Seite. Da die auf diese Art ermittelte
+             * Wenn nein, so zielt der Link auf die erstbeste, dem Produkt zugeordnete Seite die Published ist. Da die auf diese Art ermittelte
              * Seiten-ID allerdings eine Hauptsprachseiten-ID ist, muss zu dieser Seite die Seite ermittelt werden, die der
              * Sprache der aktuell aufgerufenen Seite entspricht.
              <--*/
-            $MainLanguagePageIDForLink = $this->_pages[0];
+            $MainLanguagePageIDForLink = null;
+            foreach ($this->_pages as $int_pageID) {
+                $pageInfo = \PageModel::findWithDetails($int_pageID);
+                if ($pageInfo->published == "1") {
+                    $MainLanguagePageIDForLink = $int_pageID;
+                    break;
+                }
+            }
+
             $languagePages = ls_shop_languageHelper::getLanguagePages($MainLanguagePageIDForLink);
             $currentLanguagePageIDForLink = $languagePages[$objPage->language]['id'];
 
@@ -2367,6 +2375,12 @@ This method can be used to call a function hooked with the "callingHookedProduct
         if ($objPage->id == ls_shop_languageHelper::getLanguagePage('ls_shop_searchResultPages', false, 'id')) {
             $addReturnPageToUrl = '/calledBy/searchResult';
         }
+
+        //redirect to productNotFound Published Page if no categorie/site is set for this product
+        if(empty($MainLanguagePageIDForLink)){
+            return ls_shop_languageHelper::getLanguagePage('ls_shop_productNotFound');
+        }
+
 
         $this->ls_linkToProduct = \Controller::generateFrontendUrl($objProductPage->row(), '/product/'.$this->_alias.($var_useVariantAliasOrID ? '/selectVariant/'.$var_useVariantAliasOrID : '').$addReturnPageToUrl);
 
