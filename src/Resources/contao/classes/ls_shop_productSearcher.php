c31f29566f6c33686c90851ee10c53de73c517f4
Rainer Blechinger <57102323+superMi88@users.noreply.github.com>
2025-05-23 13:58:44 +0200

    fix price sorting product list (#479)
    * fix price for product search
    
    * add the cheapest price as raw value
    
    * fix price value
    
    * optimize code
    
    * fix getter
    
    * update CHANGELOG.md

--- src/Resources/contao/classes/ls_shop_productSearcher.php
+++ src/Resources/contao/classes/ls_shop_productSearcher.php
@@ -2095,18 +2095,17 @@ class ls_shop_productSearcher
                     $sortingTypeFlag = SORT_NUMERIC;
                     foreach ($arrProductsAfterFilter as $k => $arrProduct) {
                         /*
-                         * If the displayPrice already exists because it has been
-                         * calculated before for the filter, we use it.
-                         * If not, we calculate it now.
+                         * Calculates the cheapest price for this product, checks variants and scale prices
                          */
-                        if (isset($arrProduct['price'])) {
-                            $arrOrder[$k] = $arrProduct['price'];
-                        } else {
-                            if ($this->bln_useGroupPrices) {
-                                $arrProduct = $this->updateProductRowWithGroupPrice($arrProduct);
-                            }
+                        $objProduct = ls_shop_generalHelper::getObjProduct($arrProduct['id']);
 
-                            $arrOrder[$k] = ls_shop_generalHelper::getDisplayPrice($arrProduct['lsShopProductPrice'], $arrProduct['lsShopProductSteuersatz']);
+                        $bln_cheapestPriceComesFromScalePrices = $objProduct->_cheapestPriceComesFromScalePrices;
+                        $float_cheapestPrice = $objProduct->_cheapestPriceRaw;
+
+                        if (!$bln_cheapestPriceComesFromScalePrices) {
+                            $arrOrder[$k] = $objProduct->_unscaledPriceMinimumAfterTax;
+                        } else {
+                            $arrOrder[$k] = $float_cheapestPrice;
                         }
 
                         $arr_tmpProductsToSort[$k] = $arrProduct;
