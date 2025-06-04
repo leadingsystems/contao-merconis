671e971ab3bd21b0a74420bc5bd54cbf29037b98
vstuber <stuber@leadingsystems.de>
2025-06-04 16:24:07 +0200

    Use symfony session handling for merconis_modifiedDataKeys

--- src/Resources/contao/classes/ls_shop_variant.php
+++ src/Resources/contao/classes/ls_shop_variant.php
@@ -65,18 +65,13 @@ class ls_shop_variant
 		$this->ls_productID = $productID;
 		$this->ls_productVariantID = $this->ls_productID.'-'.$this->ls_ID;
 
-//        $session = System::getContainer()->get('merconis.session')->getSession();
-//        $session_modifiedDataKeys =  $session->get('merconis_modifiedDataKeys');
-//        if (isset($session_modifiedDataKeys[$this->ls_productVariantID])) {
-//            $this->modifiedDataKeys = $session_modifiedDataKeys[$this->ls_productVariantID];
-//        }
-//        $session_modifiedDataKeys[$this->ls_productVariantID] = &$this->modifiedDataKeys;
-//        $session->set('merconis_modifiedDataKeys', $session_modifiedDataKeys);
-
-        if (isset($_SESSION['merconis_modifiedDataKeys'][$this->ls_productVariantID])) {
-            $this->modifiedDataKeys = $_SESSION['merconis_modifiedDataKeys'][$this->ls_productVariantID];
+        $session = System::getContainer()->get('merconis.session')->getSession();
+        $session_modifiedDataKeys =  $session->get('merconis_modifiedDataKeys');
+        if (isset($session_modifiedDataKeys[$this->ls_productVariantID])) {
+            $this->modifiedDataKeys = $session_modifiedDataKeys[$this->ls_productVariantID];
         }
-        $_SESSION['merconis_modifiedDataKeys'][$this->ls_productVariantID] = &$this->modifiedDataKeys;
+        $session_modifiedDataKeys[$this->ls_productVariantID] = &$this->modifiedDataKeys;
+        $session->set('merconis_modifiedDataKeys', $session_modifiedDataKeys);
 
 		$this->ls_objParentProduct = &$objParentProduct;
 
