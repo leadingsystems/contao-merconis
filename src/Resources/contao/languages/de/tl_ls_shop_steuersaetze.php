d87cc32240acf807870fdfebfed937abf8c84213
Daniel Bitsch <bitsch@leadingsystems.de>
2025-06-10 16:46:45 +0200

    update CHANGELOG.md

--- src/Resources/contao/languages/de/tl_ls_shop_steuersaetze.php
+++ src/Resources/contao/languages/de/tl_ls_shop_steuersaetze.php
@@ -33,7 +33,9 @@
 	 */
 	$GLOBALS['TL_LANG']['tl_ls_shop_steuersaetze']['wildcardNotAllowed'] = 'Da Sie diesen Steuersatz bereits mindestens einem Produkt zugeordnet haben, können Sie keine dynamischen Werte verwenden.';
 
-
+    /*
+     * Error Messages
+     */
     $GLOBALS['TL_LANG']['tl_ls_shop_steuersaetze']['err_missingStopDatePeriod1'] = 'Für Gültigkeitsraum 1 wurde ein Startdatum, aber kein Enddatum eingegeben.';
     $GLOBALS['TL_LANG']['tl_ls_shop_steuersaetze']['err_missingStartDatePeriod1'] = 'Für Gültigkeitsraum 1 wurde ein Enddatum, aber kein Startdatum eingegeben.';
     $GLOBALS['TL_LANG']['tl_ls_shop_steuersaetze']['err_stopDateBeforeStartDatePeriod1'] = 'Das Enddatum von Gültigkeitsraum 1 darf nicht vor dem Startdatum liegen.';
