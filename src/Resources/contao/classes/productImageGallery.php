31e1fb8ad35d7c4d8334c4a12b015268f391aeed
vstuber <stuber@leadingsystems.de>
2025-10-15 00:28:38 +0200

    Correctly use cache pool "merconis.gallery" in productImageGallery

--- src/Resources/contao/classes/productImageGallery.php
+++ src/Resources/contao/classes/productImageGallery.php
@@ -199,7 +199,7 @@ class productImageGallery extends Frontend {
         try {
             $container = System::getContainer();
             $registry = $container->has(\LeadingSystems\ContaoCacheBundle\Cache\HandlerRegistry::class) ? $container->get(\LeadingSystems\ContaoCacheBundle\Cache\HandlerRegistry::class) : null;
-            $handler = $registry?->getHandler('default');
+            $handler = $registry?->getHandler('merconis.gallery');
         } catch (\Throwable $e) {
             $handler = null;
         }
