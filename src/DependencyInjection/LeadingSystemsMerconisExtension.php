8a03fb508be50f55de67e84962122d66def04342
vstuber <stuber@leadingsystems.de>
2025-10-16 22:10:56 +0200

    Prepend bundle-level YAML to the leading_systems_contao_cache extension to be applied

--- src/DependencyInjection/LeadingSystemsMerconisExtension.php
+++ src/DependencyInjection/LeadingSystemsMerconisExtension.php
@@ -5,10 +5,25 @@ namespace LeadingSystems\MerconisBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Yaml\Yaml;
 
-class LeadingSystemsMerconisExtension extends Extension
+class LeadingSystemsMerconisExtension extends Extension implements PrependExtensionInterface
 {
+    public function prepend(ContainerBuilder $container): void
+    {
+        $file = __DIR__ . '/../Resources/config/contao_cache.yaml';
+        if (!is_file($file)) {
+            return;
+        }
+
+        $parsed = Yaml::parseFile($file);
+        if (is_array($parsed) && isset($parsed['leading_systems_contao_cache'])) {
+            $container->prependExtensionConfig('leading_systems_contao_cache', $parsed['leading_systems_contao_cache']);
+        }
+    }
+
 	public function load(array $configs, ContainerBuilder $container)
 	{
 		$configuration = new Configuration();
