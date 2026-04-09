386b1a420d889937a63248196c732877e68d5c46
Rainer Blechinger <rainer.blechinger@web.de>
2026-04-09 11:11:12 +0200

    run 001

--- src/EventListener/SitemapListener.php
+++ src/EventListener/SitemapListener.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 
 namespace LeadingSystems\MerconisBundle\EventListener;
@@ -17,8 +18,16 @@ use Merconis\Core\ls_shop_languageHelper;
  */
 class SitemapListener
 {
+    public function __construct(
+        private readonly bool $productUrlsHandledExternally = false
+    ) {
+    }
+
     public function __invoke(SitemapEvent $event): void
     {
+        if ($this->productUrlsHandledExternally) {
+            return;
+        }
 
         $sitemap = $event->getDocument();
         $urlSet = $sitemap->childNodes[0];
